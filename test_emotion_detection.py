56d52a241fd5bdc8207db5fcc645cdf69a85aceb
Anthony <153210008+aholly1@users.noreply.github.com>
2024-08-10 16:28:02 -0400

    Update test_emotion_detection.py

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,6 +1,15 @@
+'''
+Importing the emotion_detector method from the EmotionDetection package
+'''
 from EmotionDetection.emotion_detection import emotion_detector
+'''
+Importing the UnitTest module
+'''
 import unittest
 
+'''
+A testing class that I used to test the validity of my Emotion_detector function and confirm the results
+'''
 class TestEmotionDetector(unittest.TestCase):
     def testEmotion(self):
         self.assertEqual(emotion_detector("I am glad this happened").get('dominant_emotion'), "joy")
